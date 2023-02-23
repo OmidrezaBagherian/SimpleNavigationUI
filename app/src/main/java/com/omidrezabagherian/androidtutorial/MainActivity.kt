44bfae5e6b8f9af4b0e03724a1b61d0c5c16b953
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-02-23 20:00:29 +0330

    Add Menu In ActionBar

--- app/src/main/java/com/omidrezabagherian/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/androidtutorial/MainActivity.kt
@@ -2,7 +2,11 @@ package com.omidrezabagherian.androidtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import com.omidrezabagherian.androidtutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,24 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(mainBinding.root)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_home->
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            R.id.menu_dashboard->
+                Toast.makeText(this, "Dashboard", Toast.LENGTH_SHORT).show()
+            R.id.menu_setting->
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show()
+            R.id.menu_logout->
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
