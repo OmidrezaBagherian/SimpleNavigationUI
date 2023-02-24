cf752cced8ee3bc09ca96c79f59a3188fa50d87f
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-02-24 09:53:38 +0330

    Add NavigationDrawer In ActionBar With Fragments

--- app/src/main/java/com/omidrezabagherian/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/androidtutorial/MainActivity.kt
@@ -10,9 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.omidrezabagherian.androidtutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,7 +33,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         mainBinding.navView.setupWithNavController(navController)
         initNavigationDrawer()
         managementNavigationDrawer()
@@ -80,6 +81,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController,mainBinding.drawerLayout)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
+
 }
